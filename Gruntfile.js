e4243a8e7f6bd1bd0bf4c631da1ccbc322e105ba
Joey Baker <joey@byjoeybaker.com>
2013-04-05 18:07:58 -0700

    Cleanup publish task

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,32 +54,67 @@ module.exports = function(grunt){
       gitTag: {
         command: 'git tag v<%= grunt.file.readJSON("package.json").version %>'
         , options: {
-          stderr: true
-          , stdout: true
+          stdout: true
           , failOnError: true
+        }
+      }
+      , gitRequireCleanTree: {
+        command: 'function require_clean_work_tree(){\n' +
+          ' # Update the index\n' +
+          '    git update-index -q --ignore-submodules --refresh\n' +
+          '    err=0\n' +
+
+          ' # Disallow unstaged changes in the working tree\n' +
+          '    if ! git diff-files --quiet --ignore-submodules --\n' +
+          '    then\n' +
+          '        echo >&2 "cannot $1: you have unstaged changes."\n' +
+          '        git diff-files --name-status -r --ignore-submodules -- >&2\n' +
+          '        err=1\n' +
+          '    fi\n' +
+
+          ' # Disallow uncommitted changes in the index\n' +
+          '    if ! git diff-index --cached --quiet HEAD --ignore-submodules --\n' +
+          '    then\n' +
+          '        echo >&2 "cannot $1: your index contains uncommitted changes."\n' +
+          '        git diff-index --cached --name-status -r --ignore-submodules HEAD -- >&2\n' +
+          '        err=1\n' +
+          '    fi\n' +
+
+          '    if [ $err = 1 ]\n' +
+          '    then\n' +
+          '        echo >&2 "Please commit or stash them."\n' +
+          '        exit 1\n' +
+          '    fi\n' +
+          '} \n require_clean_work_tree'
+        , options: {
+          failOnError: true
         }
       }
       , gitCommitPackage: {
         command: 'git commit --amend -i package.json --reuse-message HEAD'
         , options: {
-          stderr: true
-          , stdout: true
+          stdout: true
           , failOnError: true
         }
       }
       , gitPush: {
         command: 'git push origin master --tags'
         , options: {
-          stderr: true
-          , stdout: true
+          stdout: true
           , failOnError: true
         }
       }
       , npmPublish: {
         command: 'npm publish'
         , options: {
-          stderr: true
-          , stdout: true
+          stdout: true
+          , failOnError: true
+        }
+      }
+      , npmTest: {
+        command: 'npm test'
+        , options: {
+          stdout: true
           , failOnError: true
         }
       }
@@ -93,5 +128,5 @@ module.exports = function(grunt){
   grunt.loadNpmTasks('grunt-bumpx')
 
   grunt.registerTask('test', ['simplemocha'])
-  grunt.registerTask('publish', ['jshint', 'simplemocha', 'bump:patch', 'shell:gitCommitPackage', 'shell:gitTag', 'shell:gitPush', 'shell:npmPublish'])
+  grunt.registerTask('publish', ['shell:gitRequireCleanTree', 'jshint', 'shell:npmTest', 'bump:patch', 'shell:gitCommitPackage', 'shell:gitTag', 'shell:gitPush', 'shell:npmPublish'])
 }
